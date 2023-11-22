9f8d26e6a14a84c8831484b98ba5fcf4d2456412
Abdul Muyeed <abdulmuyeed47@gmail.com>
2023-11-22 17:48:17 +0600

    unnecessary return in bisection removed

--- Bisection.cpp
+++ Bisection.cpp
@@ -1,19 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define EPSILON 0.01
 
+// class for Bisection Method
 class Bisection
 {
+    // equation is x^3 - x^2 + 5
     public:
         static double func(double x) {
         return x * x * x - x * x + 5;
     }
+
+    // Prints root of func(x) with error of EPSILON
     public:
     void bisection(double a, double b) {
         
         
 
         double c = a;
-        while ((b - a) >= 0.01) {
+        while ((b - a) >= EPSILON) {
             c = (a + b) / 2;
 
             if (func(c) == 0.0)
@@ -24,7 +29,6 @@ class Bisection
                 a = c;
         }
         cout << "The value of root is: " << c << endl;
-        return;
     }
        
 };
@@ -37,11 +41,16 @@ int main()
     int lb , ub;
     lb = -10000;
     ub =  10000;
+    
+
+    // finding vulue of a such that f(a) is positive
     int a = (rand() % (ub - lb + 1)) + lb;
     while (solver.func(a)<0)
     {
         a = (rand() % (ub - lb + 1)) + lb;
     }
+
+    // finding vulue of b such that f(b) is negative
     int b = (rand() % (ub - lb + 1)) + lb;
     while (solver.func(b)>=0)
     {
