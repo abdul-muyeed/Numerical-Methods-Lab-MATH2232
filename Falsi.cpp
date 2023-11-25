8411d275f8454a4001289857b22f6e33599c6a3b
Abdul Muyeed <abdulmuyeed47@gmail.com>
2023-11-25 12:27:18 +0600

    falsi method added

--- Falsi.cpp
+++ Falsi.cpp
@@ -8,29 +8,35 @@ class Bisection
     // equation is x^3 - x^2 + 5
     public:
         double func(double x) {
-       return x*x*x - x*2 - 6;
+        return x*x*x - x*2 - 6;
     }
     public:
         int random(){
-            int lb = -10000;
-            int ub =  10000;
+            int lb = -100;
+            int ub =  100;
             int a = (rand() % (ub - lb + 1)) + lb;
             return a;
         }
     // Prints root of func(x) with error of EPSILON
     public:
     void bisection(double a, double b) {
-        double x=a;
+        double x=a,prev_x=INT_MAX;
         while ((b - a) >= EPSILON) {
-            // cout << "a = " << a << " b = " << b<< " f(a) = "<<func(a) << " f(b)= "<<func(b) << endl;
-            x = (a + b) / 2;
-			// cout<<"x = "<<c<<" f(x)="<<func(x)<<endl;
+            x = (a*func(b) - b*func(a)) / (func(b) - func(a));
+            //  cout << "a = " << a << " b = " << b<< " f(a) = "<<func(a) << " f(b)= "<<func(b) <<" ";
+			//  cout<<"x = "<<x<<" f(x)="<<func(x)<<endl;
+
             if (func(x) == 0.0)
                 break;
             else if (func(x)*func(a)< 0)
                 b = x;
             else
                 a = x;
+
+            if(prev_x == x)
+                break;
+
+            prev_x = x;
         }
         cout << "The value of root is: " << x << endl;
     }
@@ -42,25 +48,23 @@ class Bisection
 int main()
 {
     Bisection solver;
-        // func(a) is nagative and func(b) is positive
-         int a = solver.random() ,b = solver.random();
-        
+    // func(a) is nagative and func(b) is positive
+    int a = solver.random() ,b = solver.random();
         
         while (solver.func(a)>0)
         {
             a = solver.random();
         }
 
-        // finding vulue of b such that f(b) is negative
+        // // finding vulue of b such that f(b) is negative
         while (solver.func(b)<0)
         {
             b = solver.random();
         }
     
-    
-    cout << "You have assumed "<< a <<" and "<< b <<endl;
+    cout << "You have assumed "<< a <<" and "<< b  <<endl;
     // cout<< "f(a) = "<< solver.func(a) << " f(b) = "<< solver.func(b) <<endl;
-   
+        
            
             solver.bisection(a, b);
          
